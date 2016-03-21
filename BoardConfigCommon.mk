ca56bdba0bf1dc474375a131d702f6e094b99016
sooti <sooti85@gmail.com>
2016-03-21 19:45:05 +0200

    remove omni stuff and disable block mode for flashing
    Change-Id: I6379ae5cc5504e5183e9b331ee99ef4f43da2150

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,7 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_CPU_VARIANT := cortex-a8
-KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-5.3/bin/"
+KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-4.8/bin/"
 
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
@@ -114,7 +114,7 @@ BOARD_BLUEDROID_VENDOR_CONF := device/samsung/aries-common/libbt_vndcfg.txt
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 BOARD_USES_MULTIPLE_SDCARD_FS := true
-TARGET_USES_BLOCK_BASED_OTA := true
+BLOCK_BASED_OTA=false
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
