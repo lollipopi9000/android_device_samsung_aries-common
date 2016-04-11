0f969be5f2f6495030726c5fcea553ec71f29a6d
Coldwindofnowhere <coldwindofnowhere@gmail.com>
2016-04-11 20:22:39 +0200

    Another try

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -21,7 +21,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.internal
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
