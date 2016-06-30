c174dadd4c66ecad0f873063dcc7f6d80311bc5c
Coldwindofnowhere <coldwindofnowhere@gmail.com>
2016-06-30 10:55:33 +0200

    Revert "wifi: don't build wifi HAL"
    This reverts commit be12708c8286e3c3ee316627f21d435d702531af.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,7 +110,6 @@ BOARD_WLAN_DEVICE           := bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-BOARD_NO_WIFI_HAL           := true
 
 # Bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/aries-common/libbt_vndcfg.txt
