5db24aa2583cb4c42dfe3ce60c37fb39eda49edf
xc-racer99 <xc-racer2@live.ca>
2016-08-25 21:25:17 -0700

    [TEMP] aries-common: Add static utilities
    Built in Omni6.0 tree.  Need to add/fix makefiles for static builds in AOSP of them

--- device_base.mk
+++ device_base.mk
@@ -64,6 +64,14 @@ PRODUCT_COPY_FILES += \
     device/samsung/aries-common/config/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
     device/samsung/aries-common/config/s3c-keypad.kcm:system/usr/keychars/s3c-keypad.kcm
 
+# Prebuilt static utilities (for updater.sh)
+PRODUCT_COPY_FILES += \
+    device/samsung/aries-common/utilities/bml_over_mtd:utilities/bml_over_mtd \
+    device/samsung/aries-common/utilities/busybox:utilities/busybox \
+    device/samsung/aries-common/utilities/erase_image:utilities/erase_image \
+    device/samsung/aries-common/utilities/flash_image:utilities/flash_image \
+    device/samsung/aries-common/utilities/make_ext4fs:utilities/make_ext4fs
+
 # Input device calibration files
 PRODUCT_COPY_FILES += \
     device/samsung/aries-common/config/mxt224_ts_input.idc:system/usr/idc/mxt224_ts_input.idc
